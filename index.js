38e2e205e8d09260b00a8ef16997eb95a1541384
Shephali Shukla <shephalishukla1@gmail.com>
2023-02-01 21:05:05 +0530

    Added eventListener for button click

---
+++ index.js
@@ -0,0 +1,7 @@
+//event listener for FIRST button: every time it gets clicked 
+document.querySelector("button")[i].addEventListener("click", buttonClick);
+
+//method: what to perform when clicked
+function buttonClick(){
+    alert("i got clicked");
+}
