a75660801c99acce1d0ef090dd1ad9ae17e14d8b
Shephali Shukla <shephalishukla1@gmail.com>
2023-02-01 21:24:36 +0530

    querySelector added for buttons

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-//listener can be replaced with anonymous function(function with no name)
-document.querySelector("button").addEventListener("click", function (){
+//this loop can create problems in case we add more buttons to website.
+var totalDrumButtons = document.querySelectorAll("button").length;
+for(var i=0;i<totalDrumButtons;i++){
+
+document.querySelectorAll("button")[i].addEventListener("click", function (){
     alert("i got clicked");
+
 });
 
-
+}
 
